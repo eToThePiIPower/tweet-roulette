7dff22a66c91b9c039208e680409d27b91b0b8e4
eToThePiIPower <etothepiipower@gmail.com>
2013-06-08 22:21:34 -0400

    Moved JavaScript to separate file

--- public/media/tr.js
+++ public/media/tr.js
@@ -1,4 +1,18 @@
-function handleClick(event)
-{
-  document.location = "/search/test"
-}
+    function handleSearch(event)
+        {
+                  var queryField = document.getElementById('tr-search-query');
+                        query = encodeURIComponent(queryField.value);
+                              document.location = "/search/" + query;
+                                  }
+
+    function handleTimeline(event)
+        {
+                  var queryField = document.getElementById('tr-timeline-query');
+                        query = encodeURIComponent(queryField.value);
+                              document.location = "/user/" + query;
+                                  }
+
+    function handleRandom(event)
+        {
+                  document.location = "/random"
+                          }
